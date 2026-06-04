8a5c2aa6a4557cae72260cbac712b3fe3d42060a
ashupal07 <palashu1517@gmail.com>
2026-06-04 23:37:55 +0530

    day 6

--- Day 6/code6_1.c
+++ Day 6/code6_1.c
@@ -1,24 +1,19 @@
 //Write a program to Convert decimal to binary. 
 #include <stdio.h>
 int main(){
-    int num,bin=0,result;
+    int num,bin=0,position=1,rem;
 
     printf("Enter the decimal number: ");
     scanf("%d",&num);
 
     int temp=num;
     while(temp>0){
-        bin=bin*10+temp%2;
+        rem=temp%2;
+        bin=bin+rem*position;
+        position*=10;
         temp/=2;
 
     }
 
-    result=temp=0;
-    while(bin>0){
-        temp=bin%10;
-        result=result*10+temp;
-        bin/=10;
-
-    }
-    printf("Binary number is :%d",result);
+    printf("Binary number is :%d",bin);
 }
